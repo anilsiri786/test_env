758acfbfec6c2f76e02eb9422b33aedc7c30d85c
Anilkuamr <anildevops786@gmail.com>
2023-07-11 11:49:08 +0530

    first commit

---
+++ src/main/java/abc/com/test_env/TestEnvApplication.java
@@ -0,0 +1,13 @@
+package abc.com.test_env;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEnvApplication.class, args);
+	}
+
+}
